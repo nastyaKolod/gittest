773825ef7f73b3a9b4e53bd535799ba3dc47fe41
nastya <nastya.kolodkevich@mail.ru>
2015-05-18 10:10:23 +0200

    log4j

--- src/test/java/com/github/GithubTest.java
+++ src/test/java/com/github/GithubTest.java
@@ -11,37 +11,35 @@ import org.openqa.selenium.firefox.FirefoxProfile;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import org.apache.commons.logging.Log;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 public class GithubTest {
-	 
 
-
+	private static final Logger logger = LogManager.getLogger(AppTest.class);
 	@Test
-	 public void loginToGithubAndCreateRepository()
-	 {
-		
-		  File path = new File("C:\\Users\\student\\AppData\\Local\\Mozilla Firefox\\firefox.exe");
-		  FirefoxBinary ffbinary = new FirefoxBinary(path);
-		  FirefoxProfile fp = new FirefoxProfile();
-		  WebDriver driver = new FirefoxDriver(ffbinary, fp);
-		  driver.get("https://github.com/login");
-		  
-		  RegestrationPage loginPage = new RegestrationPage(driver); 
-		  String name ="nastyaKolod";
-	      String pass="nastyaKolod12345";
-	      String rep_name="my";
-	  
-	      loginPage.login(name, pass);
-	      driver.get("https://github.com/new");
-	      CreateRep cr =new CreateRep(driver);
-	      cr.create(rep_name);
-	      
-		  
-		
-				  
+	public void loginToGithubAndCreateRepository() {
+	
+		logger.info("Начало теста");
+		File path = new File(
+				"C:\\Users\\student\\AppData\\Local\\Mozilla Firefox\\firefox.exe");
+		FirefoxBinary ffbinary = new FirefoxBinary(path);
+		FirefoxProfile fp = new FirefoxProfile();
+		WebDriver driver = new FirefoxDriver(ffbinary, fp);
+		driver.get("https://github.com/login");
 
+		logger.info("Страница регистрации");
+		RegestrationPage loginPage = new RegestrationPage(driver);
+		String name = "nastyaKolod";
+		String pass = "nastyaKolod12345";
+		String rep_name = "my";
 
-	 }
+		loginPage.login(name, pass);
+		driver.get("https://github.com/new");
+		CreateRep cr = new CreateRep(driver);
+		cr.create(rep_name);
+		logger.info("Завершение теста");
+
+	}
 
 }
